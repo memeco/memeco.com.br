ca902701475b5225df091511420a204e794bc973
Emerson Marques Pedro <emersonmp@gmail.com>
2024-07-29 16:07:04 -0300

    Atualizei

--- app/page.js
+++ app/page.js
@@ -7,24 +7,48 @@ export default function Page() {
         Emerson (Memeco) Marques Pedro
       </h1>
 
-      <header className="bg-gray-800 text-white p-4">
+      <header className="bg-black">
         <nav>
           <ul className="flex space-x-4 justify-center">
             <li>
-              <Link href="/">Home</Link>
+              <Link
+                href="/"
+                className="text-white hover:bg-white hover:text-black"
+              >
+                Home
+              </Link>
             </li>
             <li>
-              <Link href="/sobre">Sobre</Link>
+              <Link
+                href="/sobre"
+                className="text-white hover:bg-white hover:text-black"
+              >
+                Sobre
+              </Link>
             </li>
             <li>
-              <Link href="/redes-sociais">Redes Sociais</Link>
+              <Link
+                href="/redes-sociais"
+                className="text-white hover:bg-white hover:text-black"
+              >
+                Redes Sociais
+              </Link>
             </li>
           </ul>
         </nav>
       </header>
 
-      <main className="container mx-auto p-4">
-        <p>Este é um exemplo de página em Next.js.</p>
+      <main className="container mx-auto p-4 text-black">
+        <p>
+          Olá, eu não sou o{" "}
+          <Link
+            href="https://pt.wikipedia.org/wiki/Memeco"
+            className="text-blue-500 hover:text-gray-600"
+          >
+            Memeco
+          </Link>
+          , jogador carioca.
+        </p>
       </main>
     </div>
   );
