f0ed2d47d0f9531384e01dd7f4038b7d451696e2
Emerson Marques Pedro <emersonmp@gmail.com>
2024-07-29 12:40:10 -0300

    Segundo commit

--- app/page.js
+++ app/page.js
@@ -3,10 +3,29 @@ import Link from "next/link";
 export default function Page() {
   return (
     <div>
-      <h1 id="header">Emerson (Memeco) Marques Pedro</h1>
-      <p>Este é um exemplo de página em Next.js.</p>
+      <h1 className="text-3xl font-bold text-center mb-4">
+        Emerson (Memeco) Marques Pedro
+      </h1>
 
-      <Link href="/sobre">Sobre mim</Link>
+      <header className="bg-gray-800 text-white p-4">
+        <nav>
+          <ul className="flex space-x-4 justify-center">
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/sobre">Sobre</Link>
+            </li>
+            <li>
+              <Link href="/redes-sociais">Redes Sociais</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+
+      <main className="container mx-auto p-4">
+        <p>Este é um exemplo de página em Next.js.</p>
+      </main>
     </div>
   );
 }
